5337211def7f3e5c58ce41823bafab58849b95fe
rodribp <rodrigobonilla177@gmail.com>
2023-07-14 18:58:37 -0600

    remaking component

--- js/components.js
+++ js/components.js
@@ -9,10 +9,12 @@ const ADMIN_ID = '031e0e39187846c5b4253b7c71ec8ed6';
 
 const LNURLP_URL = DOMAIN_URL + 'lnurlp/api/v1/links';
 
+
+var invoiceHash = '';
 var formUser = document.getElementById('create-user-form');
 
 //get data from lnbits api
-const apiRequestGet = async (action, usr, paramKey) => {
+const apiRequestGet = async (action, usr) => {
     let key = '';
     let url = '';
 
@@ -22,11 +24,7 @@ const apiRequestGet = async (action, usr, paramKey) => {
             url = ADMIN_URL;
             break;
         case 2:
-            key = USR_MNG_KEY;
-            url = USR_MNG_URL;
-            break;
-        case 3:
-            key = paramKey;
+            key = ADMIN_KEY;
             url = LNURLP_URL;
             break;
         default: 
@@ -53,31 +51,9 @@ const apiRequestGet = async (action, usr, paramKey) => {
 }
 
 //post requests for lnbits api
-const apiRequestPost = async (action, usr, body, paramKey) => {
-    let key = '';
-    let url = '';
-
-    switch (usr) {
-        case 1:
-            key = ADMIN_KEY;
-            url = ADMIN_URL;
-            break;
-        case 2:
-            key = USR_MNG_KEY;
-            url = USR_MNG_URL;
-            break;
-        case 3:
-            key = ADMIN_KEY;
-            url = USR_MNG_URL;
-            break;
-        case 4:
-            key = paramKey;
-            url = LNURLP_URL;
-            break;
-        default: 
-            console.log('Specify an announced kind of user')
-            break;
-    }
+const apiRequestPost = async (action, body) => {
+    let key = ADMIN_KEY;
+    let url = ADMIN_URL;
 
     let options = {
         method: 'POST',
@@ -98,111 +74,43 @@ const apiRequestPost = async (action, usr, body, paramKey) => {
     return json;
 }
 
-//structure to create user
-const createBody = (username, walletname, email, password) => {
-    let body = {
-        'admin_id': ADMIN_ID_FOR_USR_MNG,
-        'user_name': username,
-        'wallet_name': walletname,
-        'email': email,
-        'password': password
-    }
-    return body;
-}
 
 const setQrCode = (idElement, paramText, paramWidth, paramHeight) => {
     new QRCode(idElement, {text: paramText, width: paramWidth, height: paramHeight});
 }
 
-//function to generate qrcode of the wallet
-const createQrWallet = async (idElement, usrId, paramWidth, paramHeight) => {
-    let data = await apiRequestGet('wallets', 2);
-    let url = '';
-
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].user == usrId) {
-            url = DOMAIN_URL + 'wallet?usr=' + data[i].user + '&' + 'wal=' + data[i].id;
-        }
-    }
-
-    setQrCode(idElement, url, paramWidth, paramHeight);
-}
-
 
 //function to get lnurl of user
-const getLnurlp = async (idElement, invKey, paramWidth, paramHeight) => {
-    let data = await apiRequestGet('', 3, invKey);
+const getLnurlp = async (idElement, paramWidth, paramHeight) => {
+    let data = await apiRequestGet('', 2);
 
     setQrCode(idElement, data[0].lnurl, paramWidth, paramHeight);
 
     return data[0].lnurl;
 }
 
-//function to get the last user created
-const getLastUsrData = async () => {
-    let data = await apiRequestGet('wallets', 2);
-    let json = '';
-
-    for (var i = 0; i < data.length; i++) {
-        if (i == data.length - 1) {
-            json = {
-                usr: data[i].user,
-                invKey: data[i].inkey,
-                admKey: data[i].adminkey
-            }
-            
-        }
-    }
-
-    return json;
-}
-
-//function to get data from an specific usr id
-const getDataFromUsrId = async (usrId) => {
-    let data = await apiRequestGet('wallets', 2);
-    let json = '';
-
-    for (var i = 0; i < data.length; i++) {
-        if (data[i].user == usrId) {
-            json = {
-                admin: data[i].admin,
-                adminkey: data[i].adminkey,
-                id: data[i].id,
-                inkey: data[i].inkey,
-                name: data[i].name,
-                user: data[i].user
-            }
-        }
-    }
-
-    return json;
-}
-
-//function that verifies login by usrId
-const loginByUsrId = async (usrId) => {
-    let data = await getDataFromUsrId(usrId);
-    let check = false;
-
-    if (data) {
-        check = true;
-    }
-
-    return check;
-}
-
-//function to create a lnurl for a user
-const addLnurlp = async (description, min, max, admKey) => {
-    let response = await apiRequestPost('', 4, {'description': description, 'max': max, 'min': min, 'zaps': 'false', 'comment_chars': 0}, admKey);
+const createInvoice = async (amt, msg) => {
+    let response = await apiRequestPost('payments', {amount: amt, lnurl_callback: null, memo: msg, out: false, unit: 'sat'});
 
     return response;
 }
 
-//final function to create a new user and enable lnurl extension on his wallet
-const createNewUser = async (username, walletname, email, password) => {
-    await apiRequestPost('users', 2, createBody(username, walletname, email, password));
-    let data = await getLastUsrData();
-    await apiRequestPost('extensions?extension=lnurlp&userid=' + data.usr + '&active=true', 3);
-    addLnurlp('tips!', 10, 10000, data.admKey);
+document.addEventListener('DOMContentLoaded', async (e) => {
+    //let lnurl = await getLnurlp('qr-code1', 100, 100);
+    let invoice = await createInvoice(1, 'Pay for content');
+    setQrCode('qr-code1', invoice.payment_request, 200, 200);
 
-    return data.usr;
+    invoiceHash = invoice.payment_hash;
+    const interval = setInterval(verifyInvoice, 1000);
+});
+
+const verifyInvoice = async (interval) => {
+    let data = await apiRequestGet('payments/' + invoiceHash, 1);
+
+    if (data.paid == true) {
+        document.getElementById('content').innerHTML = 'UNLOCKED';
+        //clearInterval(interval);
+    }
+
+    return data.paid;
 }
